0e2fa6446b5796ec1d8d1f2d26e3a689c5fb36f9
刘家赫 <2537546514@qq.com>
2020-09-17 11:29:44 +0800

    完成个人中心开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,24 @@
 $(function () {
   getUserInfo();
+
+  // 点击按钮，实现退出功能
+  $('#btnLogout').on('click', function () {
+    // 提示用户是否确认退出
+    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (
+      index
+    ) {
+      //do something
+      // 1. 清空本地存储中的 token
+      localStorage.removeItem('token');
+      // 2. 重新跳转到登录页面
+      location.href = '/login.html';
+
+      // 关闭 confirm 询问框
+      layer.close(index);
+    });
+  });
 });
+var layer = layui.layer;
 function getUserInfo() {
   $.ajax({
     method: 'get',
@@ -14,6 +32,7 @@ function getUserInfo() {
     },
   });
 }
+
 // 渲染用户的头像
 function renderAvatar(user) {
   // 1. 获取用户的名称
@@ -32,20 +51,3 @@ function renderAvatar(user) {
     $('.text-avatar').html(first).show();
   }
 }
-
-var layer = layui.layer;
-
-// 点击按钮，实现退出功能
-$('#btnLogout').on('click', function () {
-  // 提示用户是否确认退出
-  layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
-    //do something
-    // 1. 清空本地存储中的 token
-    localStorage.removeItem('token');
-    // 2. 重新跳转到登录页面
-    location.href = '/login.html';
-
-    // 关闭 confirm 询问框
-    layer.close(index);
-  });
-});
